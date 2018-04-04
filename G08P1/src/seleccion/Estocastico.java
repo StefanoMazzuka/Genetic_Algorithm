242e83c7ef049187b79367d163b5125a413bfc45
usuario_local <usuario_local@PTO1110>
2018-04-04 17:00:46 +0200

    No ha subido todas la funciones

---
+++ G08P1/src/seleccion/Estocastico.java
@@ -0,0 +1,13 @@
+package seleccion;
+
+import base.AlgoritmoGenetico;
+
+public class Estocastico extends Seleccion {
+
+	@Override
+	public void ejecutar(AlgoritmoGenetico ag) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ G08P1/src/seleccion/Torneo.java
@@ -0,0 +1,67 @@
+package seleccion;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import base.AlgoritmoGenetico;
+import base.Cromosoma;
+
+public class Torneo extends Seleccion {
+
+	@Override
+	public void ejecutar(AlgoritmoGenetico ag) {
+		// TODO Auto-generated method stub
+		ArrayList<Cromosoma> pob = ag.getPoblacion();
+		ArrayList<Cromosoma> pobTrio = new ArrayList<Cromosoma>();
+		ArrayList<Cromosoma> pobSeleccionada = new ArrayList<Cromosoma>();
+		double mejor = 0.0;
+		Cromosoma mejorCromosoma = null;
+		Random r = new Random();
+		int cualToca = 0;
+		
+		/*
+		 * Recorremos el tamaño de la poblacion. Añadimos al pobTrio el trio de cromosomas y comprobamos
+		 * cual es el mejor de esos tres para añadirlo a la pobSeleccionada. 
+		 */
+		for (int j = 0; j < ag.getNumeroGeneraciones(); j++) {
+			
+			/*
+			 * Elegimos el trio al azar
+			 */
+			for (int i = 0; i < 3; i++) {
+				cualToca = r.nextInt(ag.getlPoblacion()) ;
+				pobTrio.add(pob.get(cualToca).copy());
+				
+				/*
+				 * Comprobamos cual es el mejor elemento del trio
+				 */
+				if ( mejor < pobTrio.get(i).fitness[0]) {
+					mejor = pobTrio.get(i).fitness[0];
+					mejorCromosoma =  pobTrio.get(i).copy();
+				}
+			}
+			
+			/*
+			 * Añadimos el cromosoma a la poblacion seleccionada
+			 */
+			pobSeleccionada.add(mejorCromosoma);
+			
+			/*
+			 * Limpiamos el array de pobTrio
+			 */
+			for (int i = 0; i < 3; i++) {
+				pobTrio.remove(0);
+				
+			}
+			
+			/*
+			 * Seteamos la poblacion orginal
+			 */
+			ag.setPoblacion(pobSeleccionada);
+
+		}
+		
+		
+	}
+
+}
